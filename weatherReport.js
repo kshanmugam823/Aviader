ebbbf77dfdefb71e1324d31bc90f3ec30bce42df
kshanmugam823 <71803022+kshanmugam823@users.noreply.github.com>
2021-11-30 18:21:04 -0500

    Add files via upload

--- weatherReport.js
+++ weatherReport.js
@@ -1,40 +1,97 @@
-document.addEventListener('DOMContentLoaded', bindButtons);
+var button = document.querySelector('#getWeather');
+var input = document.querySelector('#zip');
+var temp = document.querySelector('#mainTemp');
+var humidity = document.querySelector('#weatherHumidity');
+var description = document.querySelector('#weatherDescript');
 
-function bindButtons() {
-	document.getElementById('getWeather').addEventListener('click', function(event){
-		var req = new XMLHttpRequest();
-		var appID = "&appid=7078299be27ee986f92f04a37191b0f3";          //My API key when I signed up for an account
-		var zip = document.getElementById("zip").value;
-        var city = document.getElementById("city").value;
-        var payload;
+var mapDisplay = document.querySelector('embedMap')
 
-        if(zip == 5)    //Use zip code if available
-		{
-            payload = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us" + appID + '&units=imperial';      //Add imperial to query to get imperial units (from documentation)
-        }
-        else
-        {
-            payload = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",us" + appID + '&units=imperial';
-        }
-		req.open("GET", payload, true);                             //Set third parameter to true to send asynchronous request
+var birdOne = document.getElementById('cardinal');
+var birdTwo = document.getElementById('robin');
+var birdThree = document.getElementById('jay');
 
-		req.addEventListener('load', function() {                   //Want the function to execute once everything loads
-			if (req.status >= 200 && req.status < 400) {
-				var response = JSON.parse(req.responseText);
-				displayWeather(response);                       //Call the function to display the information
-			} else {
-				console.log("error");
-			}
-    });
-		req.send();
-		event.preventDefault();
-});
-}
 
-function displayWeather(response)
-{
-    document.getElementById("mainTemp").textContent = response.main.temp;               //display the weather data
-    document.getElementById("weatherHumidity").textContent = response.main.humidity;
-    document.getElementById("weatherSpeed").textContent = response.wind.speed;
-    document.getElementById("sunriseStart").textContent = response.sys.sunrise;
-}
+button.addEventListener('click', function(){
+
+	// Fetch request to weather service
+	fetch('http://flip1.engr.oregonstate.edu:57239/?zip='+ input.value)
+		.then(response => response.json())
+		.then(data => {
+			var tempValue = data['main']['temp'];
+			var humValue = data['main']['humidity'];
+			var descriptValue = data['weather'][0]['description'];
+
+			temp.innerHTML = tempValue;
+			humidity.innerHTML = humValue;
+			description.innerHTML	= descriptValue;
+			input.value = "";
+		})
+	
+	// Fetch request for IoT MQTT broker service
+	fetch('https://bko7deq544.execute-api.us-east-2.amazonaws.com/dev/scitizen', {
+		method: 'GET',
+		mode: 'cors',
+		headers: {
+			'authorization-token': '39dfjlekjaoviIEJ33To',
+		},
+		}
+		).then(response => response.json()).then(data => console.log(data))
+	
+	// Fetch request for map service
+	fetch('https://mapservice-stumpfjo.uw.r.appspot.com/map', {
+		method: 'POST'
+		})
+		.then(response => response.json())
+		.then(data => {
+			var mapInfo = data
+			mapDisplay.innerHTML = mapInfo;
+		})
+	
+	// Fetch request for common birds
+	fetch('http://flip3.engr.oregonstate.edu:64234/scraper', {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+		},
+		mode: "no-cors",
+		body: JSON.stringify({
+			PageURL1: 'https://www.linvilla.com/wp-content/uploads/2019/01/The-Northern-Cardinal-Bird.jpg'
+		})
+		}).then(response => response.json())
+		.then(data => {
+			birdOne.innerHTML = '<img src=' + PageURL1 + 'alt="" />';
+		});
+	
+	fetch('http://flip3.engr.oregonstate.edu:64234/scraper', {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+		},
+		mode: "no-cors",
+		body: JSON.stringify({
+			PageURL2: 'https://www.nps.gov/miss/learn/nature/images/GordonDietzman-20160716-8568.jpg'
+		})
+		}).then(response => response.json())
+		.then(data => {
+			birdTwo.innerHTML = '<img src=' + PageURL2 + 'alt="" />';
+		});
+
+	fetch('http://flip3.engr.oregonstate.edu:64234/scraper', {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+		},
+		mode: "no-cors",
+		body: JSON.stringify({
+			PageURL3: 'https://www.allaboutbirds.org/guide/assets/photo/311635911-720px.jpg'
+		})
+		}).then(response => response.json())
+		.then(data => {
+			birdTwo.innerHTML = '<img src=' + PageURL3 + 'alt="" />';
+		});
+	
+
+})
